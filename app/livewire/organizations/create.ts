1942f054b47361b35209dc8f827f6030378a2c04
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-08-21 11:53:13 +0300

    wip

--- app/livewire/organizations/create.ts
+++ app/livewire/organizations/create.ts
@@ -1,7 +1,43 @@
+import Organization from '#models/organization'
+import vine from '@vinejs/vine'
 import { Component, title } from 'adonisjs-livewire'
+
+const validator = vine.compile(
+  vine.object({
+    name: vine.string().maxLength(100),
+    email: vine.string().email().normalizeEmail().nullable(),
+    phone: vine.string().maxLength(150).nullable(),
+    address: vine.string().maxLength(150).nullable(),
+    city: vine.string().maxLength(150).nullable(),
+    region: vine.string().maxLength(150).nullable(),
+    country: vine.string().maxLength(150).nullable(),
+    postalCode: vine.string().maxLength(150).nullable(),
+  })
+)
 
 @title('Create Organization')
 export default class Create extends Component {
+  form = {
+    name: null,
+    email: null,
+    phone: null,
+    address: null,
+    city: null,
+    region: null,
+    country: null,
+    postalCode: null,
+  }
+
+  async store() {
+    await Organization.create({
+      ...(await validator.validate(this.form)),
+      accountId: this.ctx.auth.user!.id,
+    })
+    this.ctx.session.flash('success', 'Organization created.')
+    this.skipRender()
+    this.redirect('/organizations', true)
+  }
+
   async render() {
     return this.view.render('livewire/organizations/create')
   }
--- app/livewire/organizations/edit.ts
+++ app/livewire/organizations/edit.ts
@@ -1,19 +1,61 @@
 import Organization from '#models/organization'
+import vine from '@vinejs/vine'
 import { Component, title } from 'adonisjs-livewire'
+
+const validator = vine.compile(
+  vine.object({
+    name: vine.string().maxLength(100),
+    email: vine.string().email().normalizeEmail().nullable(),
+    phone: vine.string().maxLength(150).nullable(),
+    address: vine.string().maxLength(150).nullable(),
+    city: vine.string().maxLength(150).nullable(),
+    region: vine.string().maxLength(150).nullable(),
+    country: vine.string().maxLength(150).nullable(),
+    postalCode: vine.string().maxLength(150).nullable(),
+  })
+)
 
 @title('Edit Organization')
 export default class Edit extends Component {
-  declare id: number
+  declare organization: Organization
 
-  mount({ id }: any) {
-    this.id = id
+  form: any = {
+    name: null,
+    email: null,
+    phone: null,
+    address: null,
+    city: null,
+    region: null,
+    country: null,
+    postalCode: null,
+  }
+
+  async mount({ id }: any) {
+    this.organization = await Organization.findOrFail(id)
+
+    this.form = {
+      name: this.organization.name,
+      email: this.organization.email,
+      phone: this.organization.phone,
+      address: this.organization.address,
+      city: this.organization.city,
+      region: this.organization.region,
+      country: this.organization.country,
+      postalCode: this.organization.postalCode,
+    }
+  }
+
+  async update() {
+    this.organization.merge(await validator.validate(this.form))
+    await this.organization.save()
+
+    this.ctx.session.flash('success', 'Organization updated.')
+
+    this.skipRender()
+    this.redirect('/organizations')
   }
 
   async render() {
-    const organization = await Organization.findOrFail(this.id)
-
-    return this.view.render('livewire/organizations/edit', {
-      organization,
-    })
+    return this.view.render('livewire/organizations/edit')
   }
 }
